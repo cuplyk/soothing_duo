d9fd48a11ad4f124183d39b0fbdf5f100f88f8e3
cuplyk <qgrisa@gmail.com>
2025-12-08 11:48:19 +0100

    add services and scheduling app

---
+++ services/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+#Handles service ticket creation and management
+
+# ServiceTicket, Comment models
+# Ticket submission, client dashboard views
+# App-specific URL patterns (e.g., /ticket/new, /ticket/<pk>)
